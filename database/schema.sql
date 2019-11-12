8a4147ddb9b4e1d2bba3095dd2aeb384d8bd1d37
Julia R Brooks <jrbrookshorn@gmail.com>
2019-11-12 10:42:00 -0500

    Initial commit

---
+++ database/schema.sql
@@ -0,0 +1,19 @@
+BEGIN TRANSACTION;
+
+
+CREATE TABLE street (
+  id serial,
+  name VARCHAR(50) NOT NULL,
+  side VARCHAR(50) NOT NULL,
+  neighborhood VARCHAR(50) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cleaning_day date NOT NULL,
+  
+  constraint id primary key (id)
+);
+
+  
+
+
+COMMIT;
